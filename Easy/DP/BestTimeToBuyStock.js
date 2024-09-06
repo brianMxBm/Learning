adc61351f411f585cd8b7dbbe37aa4b2555b8e02
brianMxBm <brian.melgar.mx@gmail.com>
2024-09-05 19:37:53 -0700

    [121]: Best Time To Buy & Sell Stock

---
+++ Easy/DP/BestTimeToBuyStock.js
@@ -0,0 +1,30 @@
+//leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+//@SEE: With this solution we get a time complexity of O(n) since in every case we hae to check the entire array.
+
+https: var maxProfit = function (prices) {
+  let buyPrice = prices[0];
+  let profit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < buyPrice) {
+      buyPrice = prices[i];
+    }
+    profit = Math.max(profit, prices[i] - buyPrice);
+  }
+
+  return profit;
+};
+
+//@SEE It's also possible with Kadane's Algo. Refer here: https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+
+const testCase = [7, 1, 5, 3, 6, 4];
+
+const result = maxProfit(testCase);
+
+console.log(result);
